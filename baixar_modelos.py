d954aea88f8b6f67c6619b347586c6360daffda3
Alexandre Salgado <imxderr@gmail.com>
2025-05-25 22:18:13 -0300

    fix

--- baixar_modelos.py
+++ baixar_modelos.py
@@ -1,11 +1,11 @@
 import gdown
 import zipfile
 import os
+import time 
 
 file_id = '1qTP2PMuBbuxLcN8oZ609v2sj4xwLIpfX'
 zip_filename = 'models.zip'
 dest_folder = '.'
-
 download_url = f'https://drive.google.com/uc?id={file_id}'
 
 print(f"Baixando {zip_filename}...")
@@ -14,11 +14,15 @@ try:
     gdown.download(download_url, zip_filename, quiet=False)
     print(f"{zip_filename} baixado com sucesso!")
 
+    print("Aguardando 2 segundos antes de extrair...")
+    time.sleep(2) 
     print(f"\nExtraindo {zip_filename} para '{os.path.abspath(dest_folder)}'...")
-    
+
     with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
         zip_ref.extractall(dest_folder)
     print("Arquivos extraídos com sucesso!")
+
+    time.sleep(1)
 
     os.remove(zip_filename)
     print(f"{zip_filename} removido.")
